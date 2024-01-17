709cb19e812fd8af454979f856ab8806f073d808
Alex <48759429+Aurumaker72@users.noreply.github.com>
2024-01-17 17:59:47 +0100

    Stub out interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,13 +1,38 @@
-use crate::generator::Register;
-use crate::Block;
+use crate::generator::{Instruction, Register};
+use crate::Program;
 use std::collections::HashMap;
+use std::ptr::addr_of;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct Interpreter {
     registers: HashMap<Register, u64>,
     address: u64,
 }
 
-pub fn run(blocks: Vec<Block>) {
-    let interpreter = Interpreter::default();
+pub fn run(program: Program) {
+    let mut interpreter = Interpreter::default();
+
+    loop {
+        if interpreter.address >= program.len() as u64 {
+            println!("Program finished");
+            println!("{:?}", interpreter);
+            return;
+        }
+        let instruction = program[interpreter.address as usize].clone();
+
+        match instruction {
+            Instruction::Copy(dst, src) => {}
+            Instruction::Init(dst, val) => {}
+            Instruction::Jmp(addr) => {}
+            Instruction::Ret => {}
+            Instruction::Addr => {}
+            Instruction::Add(_, _, _) => {}
+            Instruction::Sub(_, _, _) => {}
+            Instruction::Mul(_, _, _) => {}
+            Instruction::Div(_, _, _) => {}
+        }
+
+        interpreter.address += 1;
+    }
 }
+
